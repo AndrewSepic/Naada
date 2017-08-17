4a2acc262fea1950d479584783d1143f09d98a82
Roy Scheffers <r.scheffers@gmail.com>
2017-08-18 00:19:05 +0200

    update clinic page with ACFs

--- wp-content/themes/naada2016/clinicpage.php
+++ wp-content/themes/naada2016/clinicpage.php
@@ -12,7 +12,9 @@ function naada_clinicBanner() {
       <!-- Calls our Call out Widget -->
       <h1> <?php the_field('hero_title'); ?></h1>
       <h3> <?php the_field('hero_description');?></h3>
-      <a href="/therapeutic-clinic/clinic-appointments/" class="naada-button orange-button large">Book An Appointment</a>
+      <a href="/therapeutic-clinic/clinic-appointments/" class="naada-button orange-button large">
+        <?php the_field('book_an_appointment'); ?>
+      </a>
     </div>
   </div>
   <?php
@@ -25,12 +27,12 @@ function naada_clinicBanner() {
 function my_custom_stuff(){
   ?>
     <div class="centerWrap">
-      <h2>How Does It Work?</h2>
+      <h2><?php the_field('how_does_it_work_title');?></h2>
       <p class="howItWorks"><?php the_field('how_does_it_work_text'); ?></p>
     </div>
 
     <div class="centerWrap">
-        <h2>Treatments</h2>
+        <h2><?php the_field('treatments_title'); ?></h2>
       <p><?php the_field('treatments_text'); ?></p>
     </div>
 
@@ -38,38 +40,53 @@ function my_custom_stuff(){
     <div class="servicesWrap">
       <div class="serviceBlock">
         <div class="serviceLeft" style="background-image: url('<?php the_field('osteo_image');?>');'">
-          <h2>Osteopathy</h2>
+          <h2><?php the_field('osteopathy'); ?></h2>
         </div><!-- serviceLeft -->
         <div class="serviceRight">
           <p><?php the_field('osteopath_text'); ?></p>
-          <a href="/therapeutic-clinic/osteopathy-appointments/" class="naada-button orange-button medium">Book An Appointment</a> <a href="/therapeutic-clinic/our-therapists/" class="naada-button orange-hollow medium">Meet Our Osteopaths</a>
+          <a href="/therapeutic-clinic/osteopathy-appointments/" class="naada-button orange-button medium">
+            <?php the_field('book_an_appointment'); ?>
+          </a>
+          <a href="/therapeutic-clinic/our-therapists/" class="naada-button orange-hollow medium">
+            <?php the_field('meet_our_osteopaths'); ?>
+          </a>
         </div><!-- serviceRight -->
       </div><!-- serviceBlock -->
 
 
       <div class="serviceBlock">
         <div class="serviceLeft" style="background-image: url('<?php the_field('yoga_therapy_image');?>');'">
-          <h2>Yoga Therapy</h2>
+          <h2><?php the_field('yoga_therapy_title'); ?></h2>
         </div><!-- serviceLeft -->
         <div class="serviceRight">
           <p><?php the_field('yoga_therapy_text'); ?></p>
-          <a href="/therapeutic-clinic/yoga-therapy-appointments/" class="naada-button orange-button medium">Book An Appointment</a> <a href="/therapeutic-clinic/our-therapists/" class="naada-button orange-hollow medium">Meet Our Therapists</a>
+          <a href="/therapeutic-clinic/yoga-therapy-appointments/" class="naada-button orange-button medium">
+            <?php the_field('book_an_appointment'); ?>
+          </a>
+          <a href="/therapeutic-clinic/our-therapists/" class="naada-button orange-hollow medium">
+            <?php the_field('meet_our_therapists'); ?>
+          </a>
         </div><!-- serviceRight -->
       </div><!-- serviceBlock -->
 
       <div class="serviceBlock">
         <div class="serviceLeft" style="background-image: url('<?php the_field('massage_therapy_image');?>');'">
-          <h2>Massage Therapy</h2>
+          <h2><?php the_field('massage_therapy_title'); ?></h2>
         </div><!-- serviceLeft -->
         <div class="serviceRight">
           <p><?php the_field('massage_therapy_text'); ?></p>
-          <a href="/therapeutic-clinic/massage-therapy-appointments/" class="naada-button orange-button medium">Book An Appointment</a> <a href="/therapeutic-clinic/our-therapists/" class="naada-button orange-hollow medium">Meet Our Therapists</a>
+          <a href="/therapeutic-clinic/massage-therapy-appointments/" class="naada-button orange-button medium">
+            <?php the_field('book_an_appointment'); ?>
+          </a>
+          <a href="/therapeutic-clinic/our-therapists/" class="naada-button orange-hollow medium">
+            <?php the_field('meet_our_therapists'); ?>
+          </a>
         </div><!-- serviceRight -->
       </div><!-- serviceBlock -->
 
       <div class="serviceBlock">
         <div class="serviceLeft" style="background-image: url('<?php the_field('sauna_image');?>');'">
-          <h2 class="sauna">Infared Sauna</h2>
+          <h2 class="sauna"><?php the_field('sauna_title'); ?></h2>
         </div><!-- serviceLeft -->
         <div class="serviceRight">
           <p><?php the_field('sauna_text'); ?></p>
