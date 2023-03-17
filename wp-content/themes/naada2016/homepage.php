7611a87591fc10258d7a3df86c9e67080fc81a37
AndrewSepic <andrew@thinkupdesign.ca>
2023-03-17 13:13:43 -0400

    Added homepage video back into homepage, added gitignore for vid folder

--- wp-content/themes/naada2016/homepage.php
+++ wp-content/themes/naada2016/homepage.php
@@ -6,15 +6,21 @@ Template Name: Homepage
 add_action( 'genesis_after_header', 'naada_videoBanner', 10 );
 function naada_videoBanner() {
   ?>
+  <div class="videoBanner">
 
+  <!-- Calls our Call out Widget -->
+  <?php dynamic_sidebar("video_callout"); ?>
+
+  <!-- Video -->
+  <video id="naadaFootage" autoplay="autoplay" muted loop poster="http://naada.staging.wpengine.com/wp-content/themes/naada2016/vid/still.jpg">
+    <source src="<?php echo get_stylesheet_directory_uri(); ?>/vid/VideoBanner3-HD.mp4" type="video/mp4">
+    <source src="<?php echo get_stylesheet_directory_uri(); ?>/vid/WebMVideo1.webm" type="video/webm">
+
+
+    Your browser doesn't support HTML5 video. Here's a <a href="#">link</a> to download the video.
+    </video>
+  </div>
   <?php
-  if (ICL_LANGUAGE_CODE == 'fr' ) {
-    // French slider
-    putRevSlider('naada-homepage-fr');
-  } else {
-    // New Slider using Slider REvolution
-    putRevSlider("naada-homepage");
-  }
 }
 ?>
 
